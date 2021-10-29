19ee1507e8a21ce703413e9f544951db7ca98ca0
ElSAofSrg <73434810+ElSAofSrg@users.noreply.github.com>
2021-10-30 01:01:48 +0700

    Add files via upload

---
+++ tambah_jurusan.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="http://cdn.jssdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="
+	sha384-+0n0xVV2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOO17+AMvyTG2x" crossorigin="anonymous">
+	<title></title>
+</head>
+<body>
+	<h3>Tambah Data JUrusan</h3>
+	<form action="proses_tambah_jurusan.php" method="post">
+		Nama Jurusan :
+		<input type="text" name="nama_jurusan" value="" class="form-control">
+		Angkatan :
+		<input type="text" name="angkatan" value="" class="form-control"><br>
+		<input type="submit" name="simpan" class="btn btn-primary">
+	</form>
+
+	<script src="http://cdn.jssdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//E1J19smozuHV6z3Iehds+3U1b9Bn9P1x0x4" crossorigin="anonymous"></script>
+</body>
+</html>
